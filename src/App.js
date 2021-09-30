508a38210725fe65e9afe964c7ba4d4a0a11de20
sakitkepala <andikapriyotama@gmail.com>
2021-09-30 23:51:33 +0700

    Styling layout & komponen notifikasi statis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import styled from "@emotion/styled";
+
+const createDummyMessages = (messageList) => {
+  return messageList.map((konten, index) => ({
+    id: index + 1,
+    message: konten,
+    timestamp: new Date().getTime() - 36000 + index * 5,
+  }));
+};
+
+const messages = createDummyMessages([
+  "Anda berhasil gabung",
+  "Teman Anda memposting komentar",
+  "Teman Anda yang lain memposting sebuah foto",
+]);
+debugger;
+
+const AppContainer = styled.div`
+  label: app-container;
+  display: grid;
+  grid-template-columns: 1fr 400px;
+  min-height: 100vh;
+
+  .notifikasi {
+    background-color: hotpink;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div>Konten</div>
+      <div className="notifikasi">
+        <h1>Bilah notifikasi</h1>
+        <NotificationList messages={messages} />
+      </div>
+    </AppContainer>
+  );
+}
+
+const NotificationListContainer = styled.div`
+  label: notification-list;
+
+  .item {
+    margin: 20px;
+    padding: 20px;
+    border-radius: 2px;
+    box-shadow: 0 1px 2px 1px rgba(0, 0, 0, 0.15);
+    background-color: #ffffff;
+    color: purple;
+  }
+`;
+
+function NotificationList({ messages }) {
+  const messagesDescending = messages.sort((a, b) =>
+    a.timestamp < b.timestamp ? 1 : -1
+  );
+  return (
+    <NotificationListContainer>
+      {messagesDescending.map(({ id, message }) => (
+        <div key={id} className="item">
+          {message}
+        </div>
+      ))}
+    </NotificationListContainer>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Global as GlobalStyles, css } from "@emotion/react";
 import App from "./App";
+
+const globalStyles = css`
+  body * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles styles={globalStyles} />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
